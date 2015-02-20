5c1326f613f09bc6f023f9c9470e3fa295ca7fd4
DJ Wilkerson <djay.wilk@gmail.com>
2015-02-20 14:24:14 -0600

    Styling for Everything
    Styling header, events, pictures, hr’s, etc.

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+    <!-- START HEADER.PHP -->
+    <head>
+        <!-- TITLES & META -->
+        <title>LLUMC's 2015 Easter Portal</title>
+        
+        <!-- STYLESHEETS -->
+        <link rel="stylesheet" type="text/css" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+        <!--<link rel="stylesheet" type="text/css" href="bower_components/bootstrap/dist/css/bootstrap-theme.min.css">-->
+        <link rel="stylesheet" type="text/css" href="css/easter-style.css">
+    </head>
+    <body>
+        <header class="container llumc-easter-header">
+            <div class="col-md-8 col-md-offset-2">
+                <img src="images/weblogo-llumceaster.svg" class="img-responsive">
+            </div>
+        </header><hr class="container header-hr">
+        
+        <div class="container llumc-easter-event-list">
+            <div class="row llumc-easter-event">
+                <div class="col-md-4 llumc-event-img"><img src="images/Family-savings-workshop-screen.jpg" class="img-responsive"></div>
+                <div class="col-md-8">
+                    <h3>Event Header | Event Date Month 1-2</h3><hr>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat...</p>
+                </div>
+            </div>
+            <div class="row llumc-easter-event">
+                <div class="col-md-4 llumc-event-img"><img src="images/Family-savings-workshop-screen.jpg" class="img-responsive"></div>
+                <div class="col-md-8">
+                    <h3>Event Header | Event Date Month 1-2</h3><hr>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat...</p>
+                </div>
+            </div>
+            <div class="row llumc-easter-event">
+                <div class="col-md-4 llumc-event-img"><img src="images/Family-savings-workshop-screen.jpg" class="img-responsive"></div>
+                <div class="col-md-8">
+                    <h3>Event Header | Event Date Month 1-2</h3><hr>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat...</p>
+                </div>
+            </div>
+        </div>
+    
+        <footer>
+
+        </footer>
+        <script src="bower_components/jquery/dist/jquery.min.js"></script>
+        <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    </body>
+    <!-- END FOOTER.PHP -->
+</html>
